1c89a5affbcf3d6854ffd8471759d15f4658ea5c
Dan Ratner <dan@danratner.com>
2023-08-30 13:48:04 -0500

    chat tests

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ generated, err := gpt.Generate(prompt)
 If you want to test this module, copy the file testconfig-sample.json to testconfig.json and replace the org id and api key with your settings. You can change anything else as well, but you'll need a working API key.
 
 Run ```go test -v``` to see verbose output.
+
+BE AWARE TESTS ARE ON THE LIVE API. You will be using tokens, although max_tokens is set to 100 for each query. Total usage for the test suite is around 1,000 tokens.
--- gogpt_test.go
+++ gogpt_test.go
@@ -68,4 +68,53 @@ func TestGenerateWithFunctions(t *testing.T) {
 
 func TestGenerateChat(t *testing.T) {
 
+	gpt1, err := buildTestQueryHelper()
+
+	if err != nil {
+		t.Errorf("Error building test query: %v", err)
+	}
+
+	gpt2, err := buildTestQueryHelper()
+
+	if err != nil {
+		t.Errorf("Error building test query: %v", err)
+	}
+
+	generated, err := gpt1.AddMessage(ROLE_SYSTEM, "You are a bumbling but confident French detective talking to your superintendant.").AddMessage(ROLE_USER, "Can you solve the great train robbery?").Generate()
+
+	if err != nil {
+		t.Errorf("Error generating: %v", err)
+	}
+
+	t.Logf("GPT1: %s\n", generated.Choices[0].Message.Content)
+
+	generated, err = gpt2.AddMessage(ROLE_SYSTEM, "You are a serious and dour English police superintendant talking to a detective. You asked the detective to solve the great train robbery.").AddMessage(ROLE_USER, generated.Choices[0].Message.Content).Generate()
+
+	if err != nil {
+		t.Errorf("Error generating: %v", err)
+	}
+
+	t.Logf("GPT2: %s\n", generated.Choices[0].Message.Content)
+
+	generated, err = gpt1.AddMessage(ROLE_USER, generated.Choices[0].Message.Content).Generate()
+
+	if err != nil {
+		t.Errorf("Error generating: %v", err)
+	}
+
+	t.Logf("GPT1: %s\n", generated.Choices[0].Message.Content)
+
+	generated, err = gpt2.AddMessage(ROLE_USER, generated.Choices[0].Message.Content).Generate()
+
+	if err != nil {
+		t.Errorf("Error generating: %v", err)
+	}
+
+	t.Logf("GPT2: %s\n", generated.Choices[0].Message.Content)
+
+	t.Logf("Query1: %+v", gpt1)
+
+	t.Logf("Query2: %+v", gpt2)
+
+	t.Logf("Generated: %+v", generated)
 }
