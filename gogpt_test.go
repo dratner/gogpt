7f739eaac764ad0c70f5d8823975f66466f21e76
Dan Ratner <dan@danratner.com>
2023-08-30 13:14:21 -0500

    simplified testing with helper

--- gogpt_test.go
+++ gogpt_test.go
@@ -12,26 +12,44 @@ type TestConfig struct {
 	GptOrgId   string `json:"gpt_org_id"`
 }
 
-func TestGenerate(t *testing.T) {
+/*
+	Simple helper function to build a test query.
+*/
+
+func buildTestQueryHelper() (*GoGPTQuery, error) {
 
 	f := "./testconfig.json"
 	file, err := os.ReadFile(f)
 
 	if err != nil {
-		t.Errorf("Error loading config: %v", err)
+		return nil, err
 	}
 
 	conf := new(TestConfig)
 	err = json.Unmarshal([]byte(file), &conf)
 
 	if err != nil {
-		t.Errorf("Error loading config: %v", err)
+		return nil, err
 	}
 
 	gpt := NewGoGPTQuery(conf.GptKey)
 	gpt.OrgName = conf.GptOrgName
 	gpt.OrgId = conf.GptOrgId
 	gpt.MaxTokens = 100
+
+	return gpt, nil
+}
+
+/*
+This is the simplest test - just a round-trip to the API.
+*/
+func TestGenerate(t *testing.T) {
+
+	gpt, err := buildTestQueryHelper()
+
+	if err != nil {
+		t.Errorf("Error building test query: %v", err)
+	}
 
 	t.Logf("Query: %+v", gpt)
 
@@ -42,4 +60,12 @@ func TestGenerate(t *testing.T) {
 	}
 
 	t.Logf("Generated: %+v", generated)
+}
+
+func TestGenerateWithFunctions(t *testing.T) {
+
+}
+
+func TestGenerateChat(t *testing.T) {
+
 }
